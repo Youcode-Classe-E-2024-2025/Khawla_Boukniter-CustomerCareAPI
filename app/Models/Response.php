44595229d6e4a861c855143257b161a252d86dc6
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-20 14:17:56 +0000

    Create response model and migration

---
+++ app/Models/Response.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Response extends Model
+{
+    protected $fillable = [
+        'ticket_id',
+        'user_id',
+        'content',
+        'is_internal',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+}
