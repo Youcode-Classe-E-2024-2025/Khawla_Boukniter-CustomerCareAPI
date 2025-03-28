8594c31ed9bf1854e0d33e83fc9ec70538aa24ac
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-28 11:39:44 +0000

    Ameliorer la barre de navigation

--- customer-care-frontend/src/components/Navbar.js
+++ customer-care-frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import authService from '../../services/authService';
+import authService from '../services/authService';
 
 function Navbar() {
     const navigate = useNavigate();
